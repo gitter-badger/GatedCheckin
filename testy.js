0f54a435730d33ee87dca1fd2d6b9a81bc7448e3
shardul <shardool@msn.com>
2015-09-25 02:31:58 +0530

    commit these two files

---
+++ testy.js
@@ -0,0 +1,13 @@
+/*globals describe:true, it:true*/
+var chai = require('chai').should(),
+    Person = require('./backend/file1.js');
+describe('Person', function () {
+    'use strict';
+    describe('greets people', function () {
+        it('can greet people', function () {
+            var p1 = new Person('Shardul'),
+                p2 = new Person('Arihant');
+            p1.greet(p2).should.equal("HelloArihant");
+        });
+    });
+});
