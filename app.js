7892580486fcfd91b440c92a422d81c13f780c80
shardul <shardool@msn.com>
2015-09-25 00:57:52 +0530

    asdfds

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 angular.module('DS',[]).controller('homeController', ['$scope', function ($scope) {
+    'use strict';
     $scope.book = {
-        title: 'Programming in C  fasd',
+        title: 'Programming in C',
         author: 'Dennis Ritchie'
     };
 }]);
