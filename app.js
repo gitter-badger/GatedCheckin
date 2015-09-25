1da65f500017eb07571e86ef1373d24d9798d008
shardul <shardool@msn.com>
2015-09-25 08:57:19 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-angular.module("DS",[]).controller("homeController", ["$scope", function ($scope) {
+angular.module("DS", []).controller("homeController", ["$scope", function ($scope) {
     "use strict";
     $scope.book = {
         title: "Programming in C",
