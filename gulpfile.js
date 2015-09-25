a4b69fb5e281670a15bda911a345101f759ca591
shardul <shardool@msn.com>
2015-09-25 09:59:16 +0530

    added istanbul codecoverage

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     lint = require('gulp-jshint'),
-    tests = require('gulp-mocha');
+    tests = require('gulp-mocha'),
+    istanbul = require('gulp-istanbul');
 
 gulp.task('lint', function () {
     'use strict';
@@ -19,4 +20,18 @@ gulp.task('testSuite', function () {
     'use strict';
     return gulp.src('testy.js')
         .pipe(tests());
+});
+
+gulp.task('testCoverage', function (cb) {
+    'use strict';
+  gulp.src(['lib/**/*.js', 'main.js'])
+    .pipe(istanbul()) // Covering files
+    .pipe(istanbul.hookRequire()) // Force `require` to return covered files
+    .on('finish', function () {
+      gulp.src(['testy.js'])
+        .pipe(tests())
+        .pipe(istanbul.writeReports()) // Creating the reports after tests ran
+        .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } })) // Enforce a coverage of at least 90%
+        .on('end', cb);
+    });
 });
